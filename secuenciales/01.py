dd8eec3a6640f7c706f9a63dc2046167749e049d
oaemdl <oaemdl@gmail.com>
2023-02-15 10:43:39 -0500

    proyecto Python

---
+++ secuenciales/01.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+varones = int(input("Varones : "))
+mujeres = int(input("Mujeres : "))
+
+total = varones + mujeres
+pvarones = varones * 100.0 / total
+pmujeres = mujeres * 100.0 / total
+
+print(f"%Varones = { format(pvarones, '.2f')} %")
+print(f"%Mujeres = { format(pmujeres, '.2f')} %")
+
+print()
