ca75cfda390b959dc5e207ef9e2a46de2f642915
oaemdl <oaemdl@gmail.com>
2023-03-02 12:19:46 -0500

    python

--- varios/TR 2023-1.py
+++ varios/TR 2023-1.py
@@ -1,18 +1,46 @@
 from tkinter import *
 from tkinter import ttk
 
+producto_descripcion = ""
+producto_unidad = ""
+producto_precio = 0.0
+producto_cantidad = 0
+total = 0.0
+
+
 def btnNuevo_Click() :
     txtCodigo.config(state='normal')
     txtCodigo.focus()
 
 def txtCodigo_Enter(*args) :
+    global producto_descripcion, producto_unidad
+    global producto_precio, producto_cantidad, total
+
     txtCantidad.config(state='normal')
     txtCantidad.focus()
-    #Buscar en archivo de texto el producto
+    
+    productos = open ('varios\productos.txt','r')
+    for linea in productos.readlines() :
+        producto = linea.split(',')
+        producto_descripcion = producto[1]
+        producto_unidad = producto[2]
+        producto_precio = producto[3]
+        producto_cantidad = producto[4]
+
+        if txtCodigo.get() == producto[0] :
+            lblProducto.set( f"Producto : {producto_descripcion}\nPrecio : S/ { producto_precio }" )
+            break
+    else : productos.close()
+
 
 def txtCantidad_Enter(*args) :
-    tblDetalle.insert('', END, text=svCodigo.get(), values=( '','','',svCantidad.get(),''))
-    
+    global producto_descripcion, producto_unidad
+    global producto_precio, producto_cantidad, total
+
+    subTotal = float(producto_precio) * int(svCantidad.get())
+    total += subTotal
+    svTotal.set(f"Total : S/ { format(total,'.2f') }")    
+    tblDetalle.insert('', END, text=svCodigo.get(), values=( producto_descripcion,producto_unidad,svCantidad.get(),producto_precio, format(subTotal,'.2f') ) )
 
     txtCodigo.config(state='disabled')
     txtCantidad.config(state='disabled')
@@ -25,7 +53,7 @@ window.title("Ferreteria el Tornillo Feliz")
 window.resizable(0,0)
 #window.iconbitmap("logo.ico")
 
-frame = Frame(window, width=600, height=400 )
+frame = Frame(window, width=600, height=600 )
 frame.pack()
 
 Label(frame, text='Dni : ', anchor=W).place(x=30, y=30, width=80, height=25)
@@ -71,18 +99,20 @@ tblDetalle = ttk.Treeview(frame, columns=('id','Descripcion','Unidad','Cantidad'
 tblDetalle.place(x=20, y=220, width=550, height=300)
 tblDetalle.column('#0', width=50)
 tblDetalle.column('#1', width=150)
-tblDetalle.column('#2', width=50)
-tblDetalle.column('#3', width=50)
+tblDetalle.column('#2', width=100)
+tblDetalle.column('#3', width=80)
 tblDetalle.column('#4', width=80)
 tblDetalle.column('#5', width=80)
 
+tblDetalle.heading('#0', text='ID')
 tblDetalle.heading('#1', text='Descripción')
 tblDetalle.heading('#2', text='Unidad')
 tblDetalle.heading('#3', text='Cantidad')
 tblDetalle.heading('#4', text='Precio')
 tblDetalle.heading('#5', text='SubTotal')
 
-#lblTotal = Label(frame, text='Total = S/', anchor=W).place(x=300, y=280, height=25)
+svTotal = StringVar()
+Label(frame, textvariable=svTotal, anchor=W).place(x=450, y=550, height=25)
 
 
 window.mainloop()
